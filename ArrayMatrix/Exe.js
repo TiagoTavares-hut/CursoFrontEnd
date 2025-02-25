e32d310989c075e4a246178efaf2841b2f8b6f68
TiagoTavares-hut <tiagols348@gmail.com>
2025-02-25 11:26:57 -0300

    25.02.2025

---
+++ ArrayMatrix/Exe.js
@@ -0,0 +1,17 @@
+//Crie um array chamado frutas contendo os seguintes elementos: "maçã", "banana", "laranja" e "uva".
+
+let frutas = ["maca", "banana", "laranja", "uva"];
+console.log(frutas);
+//Exiba o segundo elemento do array.
+console.log(frutas[1]);
+//Adicione o fruta "melancia" ao final do array.
+frutas.push("melao");
+console.log(frutas);
+//Remova o primeiro elemento do array.
+frutas.shift();
+console.log(frutas);
+//Exiba o comprimento do array atualizado no console.
+console.log(frutas.length);
+
+//Crie um array com os números de 1 a 10.
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
